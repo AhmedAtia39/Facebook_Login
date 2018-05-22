d6f94f4749b4c86f83bbd5607ab5a8871ce85ad3
AhmedAtia39 <ahmedatia689@gmail.com>
2018-05-22 18:22:51 +0200

    first commit

---
+++ app/src/main/java/com/example/ahmed/facebooklogin/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.ahmed.facebooklogin;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
